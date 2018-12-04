b92b913689248fcae5840b338d6e504886cdd921
maryzam <zamo.code@gmail.com>
2018-12-04 22:58:43 +0300

    Render all possible basic elements for processed songs

--- draft/basic-elements.js
+++ draft/basic-elements.js
@@ -1,6 +1,7 @@
-const maxWidth = 25;
-const maxHeight = 50;
-const maxCount = 10;
+const maxWidth = 50;
+const maxHeight = 100;
+const maxCountPerRow = 100;
+const yOffset = 20;
 
 d3.json("data/all-songs.json")
 	.then((data) => {
@@ -9,39 +10,69 @@ d3.json("data/all-songs.json")
 
 		const sections = data.flatMap(item => item.sections);
 	
-		const sectionsByKey = {};
-		sections.forEach(s => {
-			sectionsByKey[s.key] = sectionsByKey[s.key] || [];
-			if (sectionsByKey[s.key].length < maxCount) {
-				sectionsByKey[s.key].push(s);
+		const counter = {}
+		const demoSections = [];
+		sections.forEach((s) => {
+			counter[s.key] = counter[s.key] || 0;
+			if (counter[s.key] < maxCountPerRow) {
+				s.order = counter[s.key];
+				demoSections.push(s);
 			}
+			counter[s.key]++;
 		});
 
-		const tempoRange = d3.extent(sections, d => d.tempo);
-		const loudnessRange = d3.extent(sections, d => d.loudness);
-
 		// 2. prepare scales
-
+		const loudnessRange = d3.extent(sections, d => d.loudness);
 		const scaleLoudness = d3.scaleLinear()
 									.domain(loudnessRange)
 									.range([0, maxWidth]);
 
 		// moderato - 110 (see https://en.wikipedia.org/wiki/Tempo)
 		// scale relative position to the basic element width
+		const moderatoTempo = 110;
+		const tempoRange = d3.extent(sections, d => d.tempo);
 		const scaleTempo = d3.scaleLinear()
-								.domain(tempoRange)
-								.range([-1, 1]);
+							.domain([moderatoTempo, tempoRange[1]])
+							.range([0, 1]);
 
-		const allKeys = Object.keys(sectionsByKey).sort((f, s) => f - s);
-		const scaleKeys = d3.scalePoint()
+		if (scaleTempo(tempoRange[0]) < -2) {
+			scaleTempo.domain([tempoRange[0], moderatoTempo]).range([-2, 0]);
+		}		
+
+		const allKeys = Object.keys(counter).sort((f, s) => f - s);
+		const scaleKey = d3.scalePoint()
 							.domain(allKeys)
-							.range([maxWidth, 0])
+							.range([0, 1])
 							.padding(1);
 
 		// render basic elements
-
+		const maxPerRow = d3.max(Object.keys(counter), (d) => counter[d]);
+		const container = d3.select("#root")
+							.append("svg")
+								.attr("width", maxWidth * maxPerRow)
+								.attr("height", (maxHeight + yOffset) * allKeys.length);
 		// add interactive generator
+		const buildItem = (d) => {
+			const itemHeight = maxHeight;
+			const itemHalfWidth = scaleLoudness(d.loudness) / 2;
+			const midPos = scaleKey(d.key) * itemHeight;
+			const slopePos = scaleTempo(d.tempo) * itemHalfWidth;
+			return `M 0 ${itemHeight} 
+					Q ${-slopePos} ${midPos} ${-itemHalfWidth} ${midPos} 
+					Q ${-slopePos} ${midPos} 0 0
+					Q ${slopePos} ${midPos} ${itemHalfWidth} ${midPos} 
+					Q ${slopePos} ${midPos}0 0 ${itemHeight}`;
+		};
 
+		container
+			.selectAll(".element")
+			.data(demoSections).enter()
+			.append("path")
+				.attr("class", "element")
+				.attr("d", (d) => buildItem(d))
+				.attr("transform", (d, i) => `translate(${maxWidth / 2 + d.order * maxWidth}, ${ (maxHeight + yOffset) * d.key })`)
+				.style("fill", "#b6c6d6")
+				.style("stroke", "steelblue");
 	})
 	.catch((err) => {
 		console.error(err);
