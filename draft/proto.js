772901cde5997b7e2fc52da66f320fdd43a922b3
maryzam <zamo.code@gmail.com>
2018-12-17 06:39:13 +0300

    Use colors, group section to make better snowflakes

--- draft/proto.js
+++ draft/proto.js
@@ -18,6 +18,8 @@ const buildItem = (d, scales) => {
 d3.json("data/all-songs.json")
 	.then((data) => {
 
+		data.forEach((song) => { groupSections(song); });
+
 		// 1. prepare data & scales
 		const scales = prepareScales(data);
 
@@ -29,6 +31,8 @@ d3.json("data/all-songs.json")
 							.append("g")
 								.attr("transform", `translate(${width / 2}, ${ height / 2})`);
 
+		//renderSong(container, data[5], scales);
+		
 		let songId = 0;		
 		const timer = setInterval(() => {
 			const song = data[songId];
@@ -44,11 +48,45 @@ d3.json("data/all-songs.json")
 		console.error(err);
 	});
 
+function groupSections(songData) {
+	// shared constants
+	const maxGroupCount = 5;
+	const maxPerGroup = 4;
+	// related to the song
+	const sectionTotal = songData.sections.length;
+	const groupTotal = sectionTotal > maxGroupCount ? maxGroupCount : sectionTotal;
+	const groupSize = Math.ceil(sectionTotal / groupTotal);
+	const maxGroupDuration = Math.ceil(songData.duration_ms / groupTotal) / 1000;
+	// gropping
+	let currentGroup = 0;
+	let currentOrder = 0;
+	let currentDuration = 0;
+	songData.sections.forEach((section, idx) => {
+
+		if (currentDuration > maxGroupDuration || currentOrder >= groupSize) {
+			currentOrder = 0;
+			currentDuration = 0;
+			currentGroup = currentGroup + 1;
+		}
+
+		section["group"] = {
+			id: currentGroup,
+			order: currentOrder,
+		};
+		
+		currentOrder = currentOrder + 1;
+		currentDuration = currentDuration + section.duration;
+	});
+
+	return songData;
+}
+
 function renderSong(container, songData, scales) {
 
 		container.selectAll("*").remove();
 
-		let pos = 0;
+		let currentPos = 0;
+		let currentGroup = 0;
 		container
 			.append("defs")
 			.append("g")
@@ -58,12 +96,16 @@ function renderSong(container, songData, scales) {
 				.append("path")
 					.attr("d", d => buildItem(d, scales))
 					.attr("transform", (d, i) => {
-						const res = `translate(0, ${ pos })`;
-						pos = pos + scales.duration(d.duration) + 2;
+						const res = `translate(0, ${ currentPos })`;
+						if (currentGroup != d.group.id) {
+							currentGroup = d.group.id;
+							currentPos = currentPos + scales.duration(d.duration) + 2;
+						}
 						return res;
 					})
 					.style("stroke", "#cbcbcb")
-					.style("fill", "#c6e1e4");
+					.style("fill", d => scales.groupColor(d.group.order))
+					.style("fill-opacity", 0.5);
 
 		const lines = container
 			.selectAll(".line")
@@ -78,7 +120,7 @@ function renderSong(container, songData, scales) {
 }
 
 function prepareScales(data) {
-		const maxHeight = height / (4);
+		const maxHeight = height / (5);
 		const maxWidth = maxHeight * 0.2;
 		const sections = data.flatMap(item => item.sections);
 		const loudnessRange = d3.extent(sections, d => d.loudness);
@@ -104,15 +146,22 @@ function prepareScales(data) {
 							.range([0, 1])
 							.padding(1);
 
-		const maxDuration = d3.max(sections, d => d.duration);
+		const maxDuration = d3.max(sections, d => d.group.order == 0 ? d.duration : 0);
 		const scaleDuration = d3.scaleLinear()
 									.domain([0, maxDuration])
 									.range([0, maxHeight]);
+
+
+		const scaleGroupColor = d3.scaleOrdinal()
+								.domain([0, 1, 2, 3, 4])
+								.range(["#8BA7B3", "#F0D6E3", "#F2BABD"]);
+
 		return {
 			loudness: scaleLoudness,
 			tempo: scaleTempo,
 			key: scaleKey,
-			duration: scaleDuration
+			duration: scaleDuration,
+			groupColor: scaleGroupColor
 		};
 };
  
