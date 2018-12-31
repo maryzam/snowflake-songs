1991f68df615e4d4b83c0b9f639c2df06857bc37
maryzam <zamo.code@gmail.com>
2018-12-31 09:02:03 +0300

    Updates

---
+++ precache-manifest.fa86996c2cde2ab661cd51cbe4399b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dce621dcd8edbcfe9602",
+    "url": "/snowflake-songs/static/js/runtime~main.dce621dc.js"
+  },
+  {
+    "revision": "60eec16c707fc80fe52c",
+    "url": "/snowflake-songs/static/js/main.60eec16c.chunk.js"
+  },
+  {
+    "revision": "fb124b0c22c5a895f225",
+    "url": "/snowflake-songs/static/js/1.fb124b0c.chunk.js"
+  },
+  {
+    "revision": "60eec16c707fc80fe52c",
+    "url": "/snowflake-songs/static/css/main.6552b54e.chunk.css"
+  },
+  {
+    "revision": "4a67f79be6cbd20e911930f742780200",
+    "url": "/snowflake-songs/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/snowflake-songs/precache-manifest.fa86996c2cde2ab661cd51cbe4399b53.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/snowflake-songs/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
